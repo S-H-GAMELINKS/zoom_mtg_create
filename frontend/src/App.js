63a3f5fabeb13e329114835755a90942b91d83c7
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-04-04 17:18:18 +0900

    add meetings list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
 
 function App() {
+  const [meetings, setMeetings] = useState([]);
+
+  useEffect(() => {
+    console.log("ZoomのMTGを作成しました");
+  }, [meetings])
+
   const postReserve = () => {
     axios.post("/zoom/mtg").then((res) => {
-      console.log(res);
+      setMeetings([...meetings, res.data.zoom_mtg_url]);
     }).catch((err) => {
       console.log(err);
     })
@@ -16,6 +23,14 @@ function App() {
       <div className="reserve-btn" onClick={postReserve}>
         ZoomのMTGを作成
       </div>
+
+      <ul>
+        {meetings.map((mtg, key) => (
+          <li id={key}>
+            <a href={mtg}>Roomに入る</a>  
+          </li>
+        ))}
+      </ul>
     </div>
   )
 
